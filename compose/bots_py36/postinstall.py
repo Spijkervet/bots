5833a45188b4939cdd5034649589a0f2825503fa
Janne Spijkervet <janne.spijkervet@gmail.com>
2019-09-14 11:04:53 +0200

    Added python 3.6 Docker

---
+++ compose/bots_py36/postinstall.py
@@ -0,0 +1,10 @@
+import configparser
+
+config = configparser.ConfigParser()
+
+config.read(r'/usr/local/lib/python3.6/site-packages/bots/config/bots.ini')
+config.set('jobqueue', 'enabled', 'True')
+config.set('dirmonitor1', 'path', '/dirmon')
+config.remove_section('dirmonitor2')
+with open(r'/usr/local/lib/python3.6/site-packages/bots/config/bots.ini', 'w') as configfile:
+    config.write(configfile)
